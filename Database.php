c31118514e51ed511debae61b3bb4af93c941b11
JTeurlings <jeroenteurlings@newman.nl>
2017-01-30 10:48:24 +0100

    Update Database.php

--- Database.php
+++ Database.php
@@ -1,1 +1,13 @@
-
+<?php
+$username=
+$token = sha1(uniqid($username, true));
+$query = $db->prepare(
+    "INSERT INTO pending_users (username, token, tstamp) VALUES (?, ?, ?)"
+);
+$query->execute(
+    array(
+        $username,
+        $token,
+        $_SERVER["REQUEST_TIME"]
+    )
+);
