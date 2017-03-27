4c57447dcd595086059b97bd164d6c8c8415241f
robinvanhoorn <robinvanhoorn@newman.nl>
2017-03-27 09:56:09 +0200

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,1 +1,15 @@
 
+$(function() {
+    $('#homeScreenContinue').click(function(){
+            $('#hoverScreen').animate({'top':'-100vh','left':'0vw'},1000, function() {
+				$("#mainPage").css("opacity","1");
+				$("#mainPage").css("height","auto");
+			});
+        });
+});
+
+function introducéFunction() {
+	$(".introducé").toggle();
+};
+
+
