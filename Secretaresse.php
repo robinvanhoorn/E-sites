fce3f09238c58c22486a6aff0c5c2170d09b7dd0
robinvanhoorn <robinvanhoorn@newman.nl>
2017-03-29 20:32:15 +0200

    Update Secretaresse.php

--- Secretaresse.php
+++ Secretaresse.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
 
     <title>Login</title>
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="styleSecretaresse.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
